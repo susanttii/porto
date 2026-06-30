cdee66a9f92809da6d7193c5de7b129c8a0edfea
susanttii <137667886+susanttii@users.noreply.github.com>
2026-06-30 13:42:53 +0700

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     description:
       "Final-year Information Systems student at Telkom University passionate about Data Analytics, Information Security, Business Process Improvement, and UI/UX Design.",
     github: "https://github.com/susanttii",
-    linkedin: "https://bit.ly/4rdj0vy",
+    linkedin: "www.linkedin.com/in/susanti-afrilia-a36174277",
     email: "susantiafrilia18@gmail.com"
   }
 
